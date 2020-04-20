76c8ba0d6b4d6939b7aa0133211bcce04352b806
ilanvys <ilanvys@gmail.com>
2020-04-20 12:23:51 +0300

    Add basic react-leaflet map component

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
-import './app.css';
+import React, { Component } from 'react'
+import './app.css'
 import globeIcon from './satlas-logo.png'
+import 'leaflet/dist/leaflet.css'
+import { Map, TileLayer } from 'react-leaflet'
+
+const stamenTonerTiles = 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png'
+const mapCenter = [39.9528, -75.1638]
+const zoomLevel = 12
 
 export default class App extends Component {
   render() {
     return (
       <div id='background'>
-        <div id='header'>
+         <div id='header'>
           <img src={globeIcon} alt='satlas' />
         </div>
-        <div id='content'></div>
+          <Map
+              ref={m => { this.leafletMap = m; }}
+              center={mapCenter}
+              zoom={zoomLevel}
+          >
+          <TileLayer
+            url={stamenTonerTiles}
+          />
+          </Map>
       </div>
-    );
+    )
   }
 }
